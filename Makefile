083ef774072db2aa47d0dde84dc51d58c6a9989d
theHacker <git@thehacker.biz>
2014-07-20 17:58:56 +0200

    Erstes Fenster und Mainloop - Fix: Makefile nutzt nun SDL2, wie eigentlich gedacht - Makefile: pedantic-Switch und C++ 2011 Standard am Compiler eingestellt

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
-SDL_CFLAGS = `sdl-config --cflags`
-SDL_LDFLAGS = `sdl-config --libs`
+SDL_CFLAGS = `sdl2-config --cflags`
+SDL_LDFLAGS = `sdl2-config --libs`
 
 BUILD_DIRECTORY = build
 
 CC = /usr/bin/g++
-CFLAGS = -Wall -Werror -g -O0 $(SDL_CFLAGS)
+CFLAGS = -std=c++11 -Wall -Werror -pedantic -g -O0 $(SDL_CFLAGS)
 LDFLAGS = $(SDL_LDFLAGS)
 
 
--- main.cpp
+++ main.cpp
@@ -3,7 +3,54 @@
 #include <cstdlib>
 
 int main(int argc, char** argv) {
-	std::cout << "Juhuu, es klappt!" << std::endl;
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) {
+		std::cerr << "SDL could not be initialized: " << SDL_GetError() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	atexit(SDL_Quit);
+	std::cout << "SDL initialisert" << std::endl;
+
+	SDL_Window *window = SDL_CreateWindow(
+		"OpenIsles", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_WINDOW_SHOWN);
+	if (window == nullptr) {
+		std::cerr << "SDL could not create window: " << SDL_GetError() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer == nullptr) {
+		std::cerr << "SDL could not create renderer: " << SDL_GetError() << std::endl;
+		SDL_DestroyWindow(window);
+		return EXIT_FAILURE;
+	}
+
+	// Mainloop
+	bool quitGame = false;
+	while (!quitGame) {
+		// Events handeln
+		SDL_Event event;
+		if (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				quitGame = true;
+				break;
+			}
+			else if (event.type == SDL_KEYDOWN) {
+				if (event.key.keysym.sym == SDLK_ESCAPE) {
+					quitGame = true;
+					break;
+				}
+			}
+		}
+
+		// Frame zeichnen
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
 	return EXIT_SUCCESS;
 }
