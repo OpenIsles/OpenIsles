4747e4578c3feab1960686721096ce5ce6defdfe
theHacker <git@thehacker.biz>
2015-08-23 10:31:02 +0200

    Buttons für Baumenü-Gruppen aktualisiert - PHP-Script angepasst, dass es mit der tileset.png und den verschiedenen Ansichten in den Gebäudegrafiken umgehen kann

--- src/php/generate-build-buttons.php
+++ src/php/generate-build-buttons.php
@@ -4,41 +4,45 @@
  */
 
 // Pfadeinstellungen (jeweils ohne Slash am Ende)
-$tileInputDir = '../../data/img/tiles';
-$buildingInputDir = '../../data/img/objects';
+$tilesetInputFile = '../../data/img/tileset.png';
+$buildingInputDir = '../../data/img/buildings';
 $outputDir = '../../data/img/gui';
 
 // Array von Buttons, die generiert werden sollen. Dateiname jeweils ohne ".png" am Ende
-// Gebäude-Geometry berechnet sich gewöhnlich aus: "40x"+(HöheBild/BreiteBild*40)+"+4+"+(47-(BreiteBild/HöheBild*40))
+// "Gebäude-geometry crop" ist zum Ausschneiden der richtigen Ansicht
+// "Gebäude-geometry placement" sind die Koordinaten, wo das Gebäude auf den Button platziert wird
+// "Tile-geometry" sind die Koordinaten der Kachel in der tileset.png
 $buttons = array(
-    //     Tilegrafik   Gebäudegrafik  Gebäude-geometry Ausgabegrafik
-    array('grass'    , 'weaponsmith', '40x40+4+7'    , 'button-add-building-craftsman'),
-    array('grass'    , 'marketplace', '40x29+4+18'   , 'button-add-building-public'),
-    array('grass'    , 'foresters'  , '40x25+4+22'   , 'button-add-building-farm'),
-    array('water'    , 'office1'    , '40x28+4+19'   , 'button-add-building-port'),
+    //     Tile-geometry   Gebäudegrafik  Gebäude-geometry Gebäude-geometry Ausgabegrafik
+    //                                    crop             placement
+    array('64x32+0+12'  , 'toolsmiths' , '128x96+0+0'   , '44x+2+14'    , 'button-add-building-craftsman'),
+    array('64x32+0+12'  , 'marketplace', '224x160+0+0'  , '44x+2+18'    , 'button-add-building-public'),
+    array('64x32+0+12'  , 'foresters'  , '128x96+0+0'   , '44x+2+13'    , 'button-add-building-farm'),
+    array('64x32+256+96', 'office1'    , '160x112+0+0'  , '44x+2+16'    , 'button-add-building-port'),
 );
+
 
 foreach($buttons as $button) {
     $shellCmd = <<<SHELLCMD
     convert -size 52x64 canvas:transparent -filter box \
-      $tileInputDir/{$button[0]}.png -geometry 44x24+2+32 -composite \
-      $buildingInputDir/{$button[1]}.png -geometry {$button[2]} -composite \
+      \( $tilesetInputFile -crop {$button[0]} \) -geometry 44x24+2+32 -composite \
+      \( $buildingInputDir/{$button[1]}.png -crop {$button[2]} \) -geometry {$button[3]} -composite \
       temp_base.png
 
     convert temp_base.png \
       \( +clone -geometry +4+8 +level-colors \#5a5a5a -channel A -fx "a=a*0.5" \) -compose DstOver -composite \
-      $outputDir/{$button[3]}.png
+      $outputDir/{$button[4]}.png
 
     convert temp_base.png \
       \( -clone 0 -channel A -blur 0x0.75 -level 0,1% +channel +level-colors \#caad14 \) -compose DstOver -composite \
       temp_base.png \( +clone -geometry +4+8 +level-colors \#5a5a5a -channel A -fx "a=a*0.5" \) \
       -delete 1 -compose DstOver -composite \
-      $outputDir/{$button[3]}-pressed.png
+      $outputDir/{$button[4]}-pressed.png
 
     rm temp_base.png
 SHELLCMD;
 
-    echo "Generating {$button[3]}...";
+    echo "Generating {$button[4]}...";
     system($shellCmd);
     echo " OK\n";
 }
