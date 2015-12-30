834ac9ddc55399437fc9c7696b6f41d2069b2f13
theHacker <git@thehacker.biz>
2015-12-30 11:53:55 +0100

    Fix: Veralteten Kommentar aktualisiert

--- src/cpp/economics/EconomicsMgr.h
+++ src/cpp/economics/EconomicsMgr.h
@@ -15,13 +15,10 @@ public:
     ~EconomicsMgr();
 
     /**
-     * @brief Führt die Produktion für ein Gebäude durch. Es wird nur für jene vergangene Ticks produziert, für die
-     * alle Eingabewaren da waren und auch nur solange das Ausgabewarenlager nicht voll ist.
+     * @brief Führt die Produktion für ein Gebäude durch.
      *
-     * Algorithmus:
-     * Es wird ermittelt, wie viel verbraucht würde, wenn in allen vergangenen Ticks die Voraussetzungen erfüllt sind.
-     * Dann wird ermittelt, wie viele Ticks lang pro Slot die Voraussetzungen wirklich erfüllt sind. Mit dem Minimum
-     * dieser Tick-Werte wird dann die Produktion durchgeführt.
+     * Wenn alle Eingabewaren da sind und das Ausgabewarenlager nicht voll ist, UND es an der Zeit ist,
+     * etwas zu produzieren, wird eine Produktion durchgeführt.
      *
      * @param building Gebäude, für welches die Produktion durchgeführt werden soll
      */
