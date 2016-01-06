73369998b8a27dfd6857aa726ee29b572b414031
theHacker <git@thehacker.biz>
2016-01-07 00:58:53 +0100

    Überarbeitete Grafik "Hafenanlagen" fürs Baumenü vorbereitet - Material "Ozean" aus tiles.blend in die common.blend verschoben - Segel in little-ship.blend ist nun vorgebacken und das Schiff als Gruppe anlegt, dass es in anderen Blender-Dateien verlinkt werden kann

--- src/blender/ships/little-ship/render.py
+++ src/blender/ships/little-ship/render.py
@@ -3,9 +3,6 @@ import math
 
 bpy.data.objects["Wasser-Helper"].hide_render = True
 
-# Baking durchführen, um Wind ins Segel zu blasen, bevor wir rendern
-bpy.ops.ptcache.bake_all(bake = True)
-
 bpy.data.scenes["Scene"].render.resolution_percentage = 100
 for i in range(0, 360, 45):
     bpy.data.objects["Setup"].rotation_euler.z = i / 180 * math.pi
