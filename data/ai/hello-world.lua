7ad45d9ec134d45c400b68819b36ba8a64d3cabf
theHacker <git@thehacker.biz>
2016-06-12 18:49:37 +0200

    Lua-Script auch mit 4 Spaces eingerückt

--- data/ai/hello-world.lua
+++ data/ai/hello-world.lua
@@ -5,8 +5,8 @@
 firstRun = true
 
 function main()
-  if firstRun == true then
-    debug("Hello World from AI :-)")
-    firstRun = false
-  end
+    if firstRun == true then
+        debug("Hello World from AI :-)")
+        firstRun = false
+    end
 end
